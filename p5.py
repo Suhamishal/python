d7964b10948c7bdc287b1551e10dce475bd422a6
Suha Mishal <115088225+Suhamishal@users.noreply.github.com>
2023-10-23 16:16:50 +0500

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,9 @@
+#Program to print each letter of python on different lines
+
+str1="python"
+i=0
+#Running a loop from 0th index till the length of string str1 which is 'python'
+while i<len(str1):
+    n=str1[i]
+    print(n)
+    i+=1
